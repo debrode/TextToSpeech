4683fd49f385b05bf5cd8c70a2ec9d2839a2dc45
Debarun Roy <debarun.dr7.roy@gmail.com>
2021-06-25 12:18:28 +0530

    added main Features(input,func,btn,eventhandler)

--- Main.py
+++ Main.py
@@ -7,6 +7,18 @@ root.title("Text-To-Speech")
 root.geometry("800x500")
 root.iconbitmap(r'favicon.ico')
 
+def talk:
+    voice=pyttsx3.init()
+    voice.say(input.get())
+    voice.runAndWait()
+    input.delete(0,END)
+
+
+input=Entry(root,font=("Helvetica",28))
+input.pack(pady=20)
+
+myBtn=Button(root,text="Speak",command=talk)
+myBtn.pack(pady=20)
 
 
 root.mainloop()
